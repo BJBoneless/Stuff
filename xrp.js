c3a90e2da214066c8265d9405e431e5e4c967533
BJBoneless <74470238+BJBoneless@users.noreply.github.com>
2020-12-28 13:17:32 -0500

    Create xrp.js

---
+++ xrp.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currencyid="52" data-base="CAD"></div>
